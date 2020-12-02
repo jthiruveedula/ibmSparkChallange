d088b1ebce9ffdf621cf3033e436e5d26f6d63c5
Jagadeesh Thiruveedula <jthiruveedula@gmail.com>
2020-12-02 19:05:46 +0530

    generic

--- main.py
+++ main.py
@@ -26,13 +26,13 @@ logging.info("Data processing stage completed")
 #a.	Create a table based COS data schema read in Step 2
 #b.	Write the contents read from the COS object in Step 2 to into this table 
 
-inst.mysqlTableLoad(tableName="employee")
+inst.mysqlTableLoad(tableName=datetime.now().strftime('Employee%Y%m%d'))
 
 logging.info("Table create with source file schema")
 
 #5.	Develop Scala code to read the same data from the database and display frist 20 lines.  Also develop code to calculate and display the following:
 
-inst.tableDataViewer(tableName="employee")
+inst.tableDataViewer(tableName=datetime.now().strftime('Employee%Y%m%d'))
 
 logging.info("Data retrived from Database")
 
@@ -40,7 +40,7 @@ logging.info("Data retrived from Database")
 #b.	Average salary in each department
 #c.	Male and female salary gap in each department
 
-inst.aggregator(tempTableName="employeeTemp")
+inst.aggregator(tempTableName=datetime.now().strftime('Employee_Temp_%Y%m%d'))
 
 logging.info("""
 a.	Gender ratio in each department
